eb434f0f288adc2c71e132e818b8790faf4069ca
hyunsang-ahn <happylovetkd@naver.com>
2022-08-11 06:45:16 +0900

    whatsapp clone start

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCqtnevpgYXh8Zt2Fbny3-adD0-5M_fyp8",
+  authDomain: "whatapp-mern-ebe48.firebaseapp.com",
+  projectId: "whatapp-mern-ebe48",
+  storageBucket: "whatapp-mern-ebe48.appspot.com",
+  messagingSenderId: "707701491468",
+  appId: "1:707701491468:web:e6e895cefb6eaaa1ca2b29"
+};
